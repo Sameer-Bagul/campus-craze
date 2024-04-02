0251674780b5842706204cb552e6597527da9b64
sameer-bagul <sameerbagul2004@gmail.com>
2024-04-02 17:28:16 +0530

    about section added

--- js/navbar.js
+++ js/navbar.js
@@ -42,3 +42,17 @@ loginBtn.addEventListener('click', () =>{
 loginClose.addEventListener('click', () =>{
    login.classList.remove('show-login')
 })
+
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } 
+  else {
+    document.getElementById("header").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
